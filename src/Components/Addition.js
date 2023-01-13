69384a358f21ed9b732104a7bbfc1097d98581e9
Courtney <courtneyrjones95@gmail.com>
2023-01-13 13:18:30 -0600

    correct

--- src/Components/Addition.js
+++ src/Components/Addition.js
@@ -8,14 +8,21 @@ const Addition = ({}) => {
     const [firstValue, setFirstValue] = useState(null)
     const [secondValue, setSecondValue] = useState(null)
     const [guess, setGuess] = useState(null)
+    const [correctCheckVisible, setCorrectCheckVisible] = useState(false)
+    const [wrongXVisible, setWrongXVisble] = useState(false)
     useEffect(() => {
         // generating numbers
-        getRandomNumber1()
-        getRandomNumber2()
+        generateQuestion()
     }, [level])
 
     const levelSetter = (value) => {
         setLevel(value)
+    }
+    const yays = ['Nice Work!', 'Good job!', 'Crushed it!', "Correct!", "OooWeee!"]
+    const wrongs = ['Try again!', 'Think again!', 'Woopsies', 'So Close!!' ]
+    const generateQuestion = () => {
+        getRandomNumber1()
+        getRandomNumber2()
     }
 
    const getRandomNumber1 = () => {
@@ -34,8 +41,29 @@ const Addition = ({}) => {
     return
 }
 
-const handleAnswer = (e) => {
+const handleAnswerInput = (e) => {
     setGuess(e.target.value)
+}
+
+const handleSubmit = (e) => {
+    e.preventDefault()
+    let correct = firstValue + secondValue
+    let answer = Number(guess)
+    console.log(correct, Number(guess) )
+
+    if(correct === answer){
+        setCorrectCheckVisible(true)
+        setGuess(null)
+        generateQuestion()
+        setTimeout(() => {
+            setCorrectCheckVisible(false)
+        }, 3000);
+    }else if (correct != answer){
+        setWrongXVisble(true)
+        setTimeout(() => {
+            setWrongXVisble(false)
+        }, 3000)
+    }
 }
 
 
@@ -45,20 +73,40 @@ const handleAnswer = (e) => {
         {level &&
             <div className='mainContainer'>
                 <div className="inGameBtns">
-                <span class="material-symbols-outlined">
+                <span className="material-symbols-outlined" onClick={() => generateQuestion()}>
                 restart_alt
+                <span className='tooltip'>New Question</span>
                 </span>
-                <span class="material-symbols-outlined">
+               
+                <span className="material-symbols-outlined" onClick={() => setLevel(null)}>
                  display_settings
+                 <span className='tooltip'>Change Level</span>
                  </span> 
                 </div>
                 <div className='mathContainer'>
+                    {!correctCheckVisible && !wrongXVisible ?
                     <div className='question'>
                     <h1><span style={{marginLeft: '80px'}}>{firstValue}</span> <br/> {`+ ${secondValue}`}</h1>
                     <hr className='equalsBar'/>
-                    <input type="number" className='answerInput' placeholder='??????' onChange={handleAnswer}/>
-                    <button className='submitBtn'>Submit</button>
-                </div>
+                    <input type="number" className='answerInput' placeholder='??????' onChange={handleAnswerInput}/>
+                    <button className='submitBtn' onClick={(e) => handleSubmit(e)}>Submit</button>
+                    </div>
+                    : correctCheckVisible && !wrongXVisible ?
+                    <div className='correct'>
+                    <span className="material-symbols-outlined">
+                    verified
+                    </span>
+                    {yays[Math.floor(Math.random() * 5)]}
+                    </div> 
+                    : !correctCheckVisible && wrongXVisible ?
+                    <div className='incorrect'>
+                    <span className="material-symbols-outlined">
+                    cancel
+                    </span>
+                    {wrongs[Math.floor(Math.random() * 5)]}
+                    </div> : null
+                    
+                    }
             </div>
         </div>
         }
