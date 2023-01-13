e76a722aba129f2ad86df0517b689abd12f9a355
Courtney <courtneyrjones95@gmail.com>
2023-01-12 20:13:51 -0600

    cleaning up

--- src/Components/Addition.js
+++ src/Components/Addition.js
@@ -5,6 +5,8 @@ import LevelSelector from './LevelSelector';
 
 const Addition = ({}) => {
     const [level, setLevel] = useState(null)
+    const [firstValue, setFirstValue] = useState(null)
+    const [secondValue, setSecondValue] = useState(null)
 
     const levelSetter = (value) => {
         setLevel(value)
@@ -14,9 +16,17 @@ const Addition = ({}) => {
         const max = level && level === 'easy' ? 10 : level && level === 'medium' ? 100 : level && level === 'hard' ? 1000 : level && level === 'hardX' ? 9000 : false
 // could refine how num is generated
        let num = max ? Math.floor(Math.random() * max) : 0
-
+        setFirstValue(num)
        return num
    }
+
+   const getRandomNumber2 = () => {
+    const max = level && level === 'easy' ? 10 : level && level === 'medium' ? 100 : level && level === 'hard' ? 1000 : level && level === 'hardX' ? 9000 : false
+// could refine how num is generated
+   let num = max ? Math.floor(Math.random() * max) : 0
+    setSecondValue(num)
+   return num
+}
 
 
 
@@ -27,7 +37,7 @@ const Addition = ({}) => {
             <div className='mainContainer'>
                 <div className='mathContainer'>
                     <div className='question'>
-                    <h1><span style={{marginLeft: '80px'}}>{getRandomNumber1()}</span> <br/> {`+ ${getRandomNumber1()}`}</h1>
+                    <h1><span style={{marginLeft: '80px'}}>{getRandomNumber1()}</span> <br/> {`+ ${getRandomNumber2()}`}</h1>
                     <hr className='equalsBar'/>
                     <input type="number" className='answerInput' placeholder='12345'/>
                 </div>
